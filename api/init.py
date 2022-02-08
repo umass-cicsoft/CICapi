80bb9117529e5a7e4792d717c8d92f712418df0b
AVS1508 <aditya.11.present@gmail.com>
2022-02-08 15:23:47 -0500

    Added email functionality

--- api/init.py
+++ api/init.py
@@ -1,18 +1,51 @@
 import firebase_admin
-from dotenv import dotenv_values
-from flask import Flask, request
+from dotenv import load_dotenv
+import os
+from flask import Flask, request, render_template
 from flask_cors import CORS
+from flask_mail import Mail, Message
 
 from api.user_registration import UserRegistration
 from api.user_attendance import UserAttendance
 
-cred_object = firebase_admin.credentials.Certificate(dict(dotenv_values("firebase-keys.env")))
-default_app = firebase_admin.initialize_app(cred_object, dict(dotenv_values("urls.env")))
+load_dotenv()
+
+cred_object = firebase_admin.credentials.Certificate(
+    {
+        "type": os.environ.get("FIREBASE_TYPE"),
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+        "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get(
+            "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
+        ),
+        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+    }
+)
+
+default_app = firebase_admin.initialize_app(
+    cred_object, {"databaseURL": os.environ.get("DATABASE_URL"),}
+)
 
 app = Flask(__name__)
 CORS(app)
+app.config.update(
+    {
+        "MAIL_SERVER": os.environ.get("MAIL_SERVER"),
+        "MAIL_PORT": os.environ.get("MAIL_SERVER"),
+        "MAIL_USERNAME": os.environ.get("MAIL_USERNAME"),
+        "MAIL_PASSWORD": os.environ.get("MAIL_PASSWORD"),
+        "MAIL_USE_TLS": False,
+        "MAIL_USE_SSL": True,
+    }
+)
+mail = Mail(app)
 
-"""Display a message welcoming the user/tester at endpoint: "https://domain.ext/"
+"""Display a message welcoming the user/tester at endpoint: "https://cicsoft-web-api.herokuapp.com/"
 
 Returns:
     response: a 2-tuple containing an object (with only a message attribute) and an HTTP response code
@@ -29,7 +62,7 @@ def main():
     return {"message": response["message"]}, response["code"]
 
 
-"""Register a new user in the database at endpoint: "https://domain.ext/user-registration"
+"""Register a new user in the database at endpoint: "https://cicsoft-web-api.herokuapp.com/user-registration"
 
 Returns:
     response: a 2-tuple containing an object (with only a message attribute) and an HTTP response code
@@ -42,6 +75,16 @@ def registerUser():
     validation = userRegistration.validate()
     if validation["status"] == "success":
         registration = userRegistration.register()
+        if registration["code"] == 200:
+            msg = Message(
+                subject="We have received your application for CICSoft!",
+                sender=("CICSoft", "cicsoftumass@gmail.com"),
+                recipients=[registration["data"]["umass_email"]],
+            )
+            msg.html = render_template(
+                "welcome.html", firstName=registration["data"]["first_name"]
+            )
+            mail.send(msg)
         return {"message": registration["message"]}, registration["code"]
     else:
         return {"message": validation["message"]}, validation["code"]
