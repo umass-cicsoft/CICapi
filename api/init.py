ef5e5cab675e93c0c8b254f4b850f50e4aadfe07
AVS1508 <aditya.11.present@gmail.com>
2022-02-08 17:41:45 -0500

    Fixed escape characters in environment key

--- api/init.py
+++ api/init.py
@@ -15,7 +15,7 @@ cred_object = firebase_admin.credentials.Certificate(
         "type": os.environ.get("FIREBASE_TYPE"),
         "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
         "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
         "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
         "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
         "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
