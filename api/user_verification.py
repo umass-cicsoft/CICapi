dd51d23fab8047e3460bdfe3d8b8a5120dd66eca
AVS1508 <aditya.11.present@gmail.com>
2022-02-16 20:47:14 -0500

    Updated verification to not send mails to already verified users

--- api/user_verification.py
+++ api/user_verification.py
@@ -10,17 +10,24 @@ class UserVerification:
             members = dict(db.reference("/members/").get())
             _id = self.requestJSON["umass_email"].replace("@umass.edu", "")
             if _id in members.keys() and members.get(_id).get("status") == "member":
-                return {
-                    "status": "success",
-                    "code": 200,
-                    "message": "Successfully sent verification code to member!",
-                    "data": {
-                        "first_name": members.get(_id).get("first_name"),
-                        "last_name": members.get(_id).get("last_name"),
-                        "umass_email": self.requestJSON["umass_email"],
-                        "otp": self.requestJSON["otp"],
-                    },
-                }
+                if "discord_id" not in members.get(_id).keys():
+                    return {
+                        "status": "success",
+                        "code": 200,
+                        "message": "Successfully sent verification code to member!",
+                        "data": {
+                            "first_name": members.get(_id).get("first_name"),
+                            "last_name": members.get(_id).get("last_name"),
+                            "umass_email": self.requestJSON["umass_email"],
+                            "otp": self.requestJSON["otp"],
+                        },
+                    }
+                else:
+                    return {
+                        "status": "error",
+                        "code": 409,
+                        "message": "Member is already verified",
+                    }
             else:
                 return {
                     "status": "error",
