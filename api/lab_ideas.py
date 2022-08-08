ae5315de2cd2f957f39623389ce9f97ef5ddd8e7
kirat692157 <45176400+kirat692157@users.noreply.github.com>
2022-08-09 04:24:33 +0530

    Added function to handle lab idea submissions

---
+++ api/lab_ideas.py
@@ -0,0 +1,37 @@
+from firebase_admin import db
+from datetime import datetime, date
+import pytz
+import uuid
+
+
+class labIdeas:
+    def __init__(self, requestJSON):
+        self.expectedFields = [
+            "first_name",
+            "last_name",
+            "idea_text"
+        ]
+        self.requestJSON = requestJSON
+
+    def idea(self):
+        try:
+            ref = db.reference("/lab_ideas/")
+            _id = str(uuid.uuid4()).replace("-", "")
+            for field in self.expectedFields:
+                ref.child(_id).child(field).set(self.requestJSON[field])
+            return {
+                "status": "success",
+                "code": 200,
+                "message": "Idea submitted successfully",
+                "data": {
+                    "first_name": self.requestJSON["first_name"],
+                    "last_name": self.requestJSON["last_name"],
+                    "idea_text": self.requestJSON["idea_text"]
+                },
+            }
+        except:
+            return {
+                "status": "error",
+                "code": 500,
+                "message": "Server failed to process idea.",
+            }
